8a22dfc28be9aea0d54cc3f468c8cd71ba778d8d
Wajahat Adil <wajahatsyed1257@gmail.com>
2024-12-14 20:24:40 +0500

    Initial setup for Campaign Management API

---
+++ src/main/java/com/adiltech/campaign_management_api/CampaignManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.adiltech.campaign_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adiltech/campaign_management_api/CampaignManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adiltech.campaign_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
